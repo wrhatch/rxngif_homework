58102438a22c10ff5261a80501270bafc2999bff
Raghu Betina <rbetina@gmail.com>
2013-10-31 15:41:11 -0500

    rails new rxngif

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rxngif::Application.config.secret_key_base = '274abab0d3fff83448f73c8aabb617dec63333b579d8132d37891156ff1839830f6ac7130e68a04ab177db010817fa3f844a38530008bc3efbcb66fd9b195207'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rxngif::Application.config.session_store :cookie_store, key: '_rxngif_session'
