92b0cc09607bfed2333f1c307de8884b7122ddef
Raghu Betina <rbetina@gmail.com>
2013-10-31 15:45:27 -0500

    Wrote some Ruby in db/seeds.rb that will pre-populate your database with a few rows real quick to save you a bunch of typing in Rails Console

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Picture.destroy_all
+
+pictures = [
+  { source: "http://www.w3.org/html/logo/downloads/HTML5_Logo_512.png", caption: "HTML5 Logo" },
+  { source: "http://upload.wikimedia.org/wikipedia/commons/f/f1/Ruby_logo.png", caption: "Ruby logo" },
+  { source: "http://upload.wikimedia.org/wikipedia/commons/1/16/Ruby_on_Rails-logo.png", caption: "Rails logo" },
+  { source: "http://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png", caption: "JavaScript logo" }
+]
+
+pictures.each do |pic|
+  p = Picture.new
+  p.source = pic[:source]
+  p.caption = pic[:caption]
+  p.save
+end
+
+puts "There are now #{Picture.count} pictures in the database."
