759504cdc1011710eca7a85ac98a55c1ef473452
Westin Hatch <whatch2014@kellogg.northwestern.edu>
2013-11-06 11:04:20 -0600

    Done
    Done

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,44 @@
 class PicturesController < ApplicationController
   def show
+  	@id = params[:id]
+	@picture = Picture.find(@id)
+	@picture_url = @picture.source
+	@caption = @picture.caption
+  end
+
+  def index
+  	@pictures = Picture.all
+  end
+
+  def new
+  end
+
+  def create
+  	newPic = Picture.new
+  	newPic.caption = params[:caption]
+  	newPic.source = params[:source]
+  	newPic.save
+  end
+
+  def destroy
+  	@id = params[:id]
+  	@picture = Picture.find(@id)
+	@picture_url = @picture.source
+	@caption = @picture.caption
+  end
+
+  def edit
+  	@id = params[:id]
+	@picture = Picture.find(@id)
+	@picture_url = @picture.source
+	@caption = @picture.caption
+  end
+
+  def update
+  	@id = params[:id]
+  	pic = Picture.find(@id)
+  	pic.caption = params[:caption]
+  	pic.source = params[:source]
+  	pic.save
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rxngif::Application.routes.draw do
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+  get("/all_pictures", { :controller => "pictures", :action => "index" })
+  get("/new_picture_form", { :controller => "pictures", :action => "new" })
+  get("/create_picture", { :controller => "pictures", :action => "create" })
+  get("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
+  get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
+  get("/update_picture/:id", { :controller => "pictures", :action => "update" })
 end
